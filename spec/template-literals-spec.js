3afd2461adfaf5d498860c2f4ad4b4237a3c62ff
jongrover <hello@jonathangrover.com>
2015-11-16 17:30:08 -0500

    change functions to arrrow functions

--- spec/template-literals-spec.js
+++ spec/template-literals-spec.js
@@ -1,8 +1,8 @@
 import { sentence1 } from '../src/template-literals'
 
-describe('Template Literals', function () {
+describe('Template Literals', () => {
 
-  it('can have variables interpolated into them', function () {
+  it('can have variables interpolated into them', () => {
     expect(sentence1).toEqual('Fuzzy Wuzzy was a bear!')
   })
 
