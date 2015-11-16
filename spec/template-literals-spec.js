7d0f6714d4ca7c09b63b9b56de05598af5f55e99
jongrover <hello@jonathangrover.com>
2015-11-16 17:25:13 -0500

    remove semicolons from spec they are not needed in es6

--- spec/template-literals-spec.js
+++ spec/template-literals-spec.js
@@ -1,9 +1,9 @@
-import { sentence1 } from '../src/template-literals';
+import { sentence1 } from '../src/template-literals'
 
 describe('Template Literals', function () {
 
   it('can have variables interpolated into them', function () {
-    expect(sentence1).toEqual('Fuzzy Wuzzy was a bear!');
-  });
+    expect(sentence1).toEqual('Fuzzy Wuzzy was a bear!')
+  })
 
-});
+})
